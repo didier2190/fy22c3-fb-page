a45a03a333c5d9209fde1ae6b09c6be78945e711
Didier Maniraho <maniraho.didier2190@gmail.com>
2022-08-08 08:57:35 -0400

    Adding javascript

---
+++ src/style.js
@@ -0,0 +1,5 @@
+
+const search_box = document.getElementById('search-box');
+const resultBox = document.getElementById('result');
+
+search_box.addEventListener('click',()=>resultBox.classList.toggle('hidden'));
